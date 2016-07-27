85a7f6d3657be7aa8af10920dbde061f94d7ef52
joe-mojo <joachim.e.rousseau+github@gmail.com>
2016-07-27 22:50:08 +0200

    Writers for Customer

--- src/main/scala/models/Customer.scala
+++ src/main/scala/models/Customer.scala
@@ -19,14 +19,15 @@ object Customer {
     )(Customer.apply _)
   }
 
-  /*TODO les writes
   implicit val writes: Writes[Customer] = {
     (
         (__ \ "firstName").write[String] and
         (__ \ "lastName").write[String] and
-        OWrites.of[Address]
+        OWrites.apply[Address] { address =>
+          Json.toJson(address).as[JsObject]
+        }
     )(unlift(Customer.unapply))
-  }*/
+  }
 }
 
 case class Address(
@@ -41,6 +42,8 @@ case class Address(
 object Address {
   //Because each attribute of Address type has a Reads defined, Reads[Address] is given for free
   implicit val reads: Reads[Address] = Json.reads[Address]
+  implicit val writes: Writes[Address] = Json.writes[Address]
+
 }
 
 //TODO add constraint on string value \d+(?:\s?[a-zA-Z]+)?|\d+-\d+
@@ -49,6 +52,9 @@ case class AddressNumber(value: String) extends AnyVal {
 }
 object AddressNumber {
   implicit val reads: Reads[AddressNumber] = Reads.of[String].map(AddressNumber(_))
+  implicit val writes: Writes[AddressNumber] = Writes[AddressNumber] { number =>
+    JsString(number.value)
+  }
   implicit def fromString(value: String): AddressNumber = AddressNumber(value)
 }
 
@@ -57,6 +63,9 @@ case class Street(value: String) extends AnyVal {
 }
 object Street {
   implicit val reads: Reads[Street] = Reads.of[String].map(Street(_))
+  implicit val writes: Writes[Street] = Writes[Street] { street =>
+    JsString(street.value)
+  }
   implicit def fromString(value: String): Street = Street(value)
 }
 
@@ -65,6 +74,9 @@ case class PostalCode(value: String) extends AnyVal {
 }
 object PostalCode {
   implicit val reads: Reads[PostalCode] = Reads.of[String].map(PostalCode(_))
+  implicit val writes: Writes[PostalCode] = Writes[PostalCode] { postalCode =>
+    JsString(postalCode.value)
+  }
   implicit def fromString(value: String): PostalCode = PostalCode(value)
 }
 
--- src/test/scala/models/CustomerSpec.scala
+++ src/test/scala/models/CustomerSpec.scala
@@ -1,7 +1,7 @@
 package models
 
 import org.scalatest.{Inside, _}
-import play.api.libs.json.{JsSuccess, Json}
+import play.api.libs.json.{JsObject, JsString, JsSuccess, Json}
 
 class CustomerSpec extends FunSpec with ShouldMatchers with Inside {
   describe("A Customer") {
@@ -46,6 +46,157 @@ class CustomerSpec extends FunSpec with ShouldMatchers with Inside {
       }
 
       validated.get should be(Customer("Paul", "Dance", Address("1337", "John F. Kennedy avenue", "42000", "Dallas", Some("Texas"), "USA")))
+    }
+
+    it("should be written as JSON with Address attributes at top-level and no state") {
+      val expectedJson =
+        """{
+          |  "firstName": "Laurent",
+          |  "lastName": "Outang",
+          |  "number": "3 Bis",
+          |  "street": "rue Pin",
+          |  "postalCode": "69042",
+          |  "city": "Apéro-Sur-Le Coudonzeur",
+          |  "country": "France"
+        }""".stripMargin
+      val expectedJsObject = Json.parse(expectedJson).as[JsObject]
+
+      Json.toJson(
+        Customer("Laurent", "Outang", Address("3 Bis", "rue Pin", "69042", "Apéro-Sur-Le Coudonzeur", None, "France"))
+      ) should be(expectedJsObject)
+
+    }
+
+    it("should be written as JSON with Address attributes at top-level and a defined state") {
+      val expectedJson =
+        """{
+          |  "firstName": "Paul",
+          |  "lastName": "Dance",
+          |  "number": "1337",
+          |  "street": "John F. Kennedy avenue",
+          |  "postalCode": "42000",
+          |  "city": "Dallas",
+          |  "country": "USA",
+          |  "state": "Texas"
+        }""".stripMargin
+      val expectedJsObject = Json.parse(expectedJson).as[JsObject]
+
+      Json.toJson(
+        Customer("Paul", "Dance", Address("1337", "John F. Kennedy avenue", "42000", "Dallas", Some("Texas"), "USA"))
+      ) should be(expectedJsObject)
+
+    }
+
+    describe("address"){
+
+      it("should read a JSON without state ") {
+        val inputJson =
+          """{
+            |  "number": "3 Bis",
+            |  "street": "rue Pin",
+            |  "postalCode": "69042",
+            |  "city": "Apéro-Sur-Le Coudonzeur",
+            |  "country": "France"
+        }""".stripMargin
+
+        val validated = Json.parse(inputJson).validate[Address]
+
+        validated should matchPattern {
+          case JsSuccess(_, _) =>
+        }
+
+        validated.get should be(Address("3 Bis", "rue Pin", "69042", "Apéro-Sur-Le Coudonzeur", None, "France"))
+      }
+
+      it("should read a JSON with defined state ") {
+        val inputJson =
+          """{
+            |  "number": "1337",
+            |  "street": "John F. Kennedy avenue",
+            |  "postalCode": "42000",
+            |  "city": "Dallas",
+            |  "country": "USA",
+            |  "state": "Texas"
+        }""".stripMargin
+
+        val validated = Json.parse(inputJson).validate[Address]
+
+        validated should matchPattern {
+          case JsSuccess(_, _) =>
+        }
+
+        validated.get should be(Address("1337", "John F. Kennedy avenue", "42000", "Dallas", Some("Texas"), "USA"))
+      }
+
+      it("should be writen as JSON without state") {
+        val expectedJson =
+          """{
+            |  "number": "3 Bis",
+            |  "street": "rue Pin",
+            |  "postalCode": "69042",
+            |  "city": "Apéro-Sur-Le Coudonzeur",
+            |  "country": "France"
+        }""".stripMargin
+        val expectedJsObject = Json.parse(expectedJson).as[JsObject]
+
+        Json.toJson(
+          Address("3 Bis", "rue Pin", "69042", "Apéro-Sur-Le Coudonzeur", None, "France")
+        ) should  be(expectedJsObject)
+      }
+
+      it("should be writen as JSON with a defined state") {
+        val expectedJson =
+          """{
+            |  "number": "1337",
+            |  "street": "John F. Kennedy avenue",
+            |  "postalCode": "42000",
+            |  "city": "Dallas",
+            |  "country": "USA",
+            |  "state": "Texas"
+        }""".stripMargin
+        val expectedJsObject = Json.parse(expectedJson).as[JsObject]
+
+        Json.toJson(
+          Address("1337", "John F. Kennedy avenue", "42000", "Dallas", Some("Texas"), "USA")
+        ) should  be(expectedJsObject)
+      }
+
+      describe("number") {
+        it("should read a JSON string"){
+          Json.parse(
+            """
+              |"3 Bis"
+            """.stripMargin).asOpt[AddressNumber] should contain(AddressNumber("3 Bis"))
+        }
+        it("should be written as a JSON string"){
+          Json.toJson(AddressNumber("3 Bis")) should be(JsString("3 Bis"))
+        }
+      }
+
+      describe("street") {
+        it("should read a JSON string"){
+          Json.parse(
+            """
+              |"rue Tabaga"
+            """.stripMargin).asOpt[Street] should contain(Street("rue Tabaga"))
+        }
+        it("should be written as a JSON string"){
+          Json.toJson(Street("rue Tabaga")) should be(JsString("rue Tabaga"))
+        }
+      }
+
+      describe("postal code") {
+        it("should read a JSON string"){
+          Json.parse(
+            """
+              |"93370"
+            """.stripMargin).asOpt[PostalCode] should contain(PostalCode("93370"))
+        }
+        it("should be written as a JSON string"){
+          Json.toJson(PostalCode("75008")) should be(JsString("75008"))
+        }
+      }
+
     }
 
   }
