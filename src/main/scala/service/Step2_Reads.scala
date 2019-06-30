7467f41364e7a5e911df28b8a2680f8345791d8d
joe-mojo <joachim.e.rousseau+github@gmail.com>
2019-06-30 22:21:13 +0200

    useless files

--- src/main/scala/service/Step2_Reads.scala
+++
@@ -1,6 +0,0 @@
-package service
-
-object Step2_Reads {
-	//TODO 2:  See models.Invoice, models.Film, models.FilmType, models.FilmEvent, models.Customer
-	// demo bonus: Show Reads for open enums (Commands) drone-game/drone-api/src/main/scala/org/jro/dronegame/api/json/DefaultReaders.scala
-}
--- src/main/scala/service/Step3_Writes.scala
+++
@@ -1,5 +0,0 @@
-package service
-
-object Step3_Writes {
-	//TODO 3:  See models.Invoice, models.Film, models.FilmType, models.FilmType, models.Customer
-}
